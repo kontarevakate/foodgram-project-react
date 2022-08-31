452b32cc05110ad570c211416f306a5908eef0c1
Екатерина Контарева <berbekaru.katerina@yandex.ru>
2022-08-31 19:16:36 +0300

    + add page_size fixed

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,6 +1,6 @@
-from django.conf import settings
+# from django.conf import settings
 from rest_framework.pagination import PageNumberPagination
 
 
 class LimitPageNumberPagination(PageNumberPagination):
-    page_size = settings.REST_FRAMEWORK.PAGE_SIZE
+    page_size = 6
