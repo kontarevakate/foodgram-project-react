f1909ae43fc819d6ced0d48c0f4bf4eeca77fd21
Екатерина Контарева <berbekaru.katerina@yandex.ru>
2022-08-31 20:34:53 +0300

    mv load_data

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -6,7 +6,7 @@ echo "Collect static..."
 python manage.py collectstatic --no-input
 
 echo "Load ingredients..."
-python load_data.py data/ingredients.json
+python manage.py load_data
 
 echo "Start foodgram..."
 gunicorn foodgram.wsgi:application --bind 127.0.0.1:8000
