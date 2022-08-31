de9d7bc666ef735db8dc307f5a2adcad142dca80
Екатерина Контарева <berbekaru.katerina@yandex.ru>
2022-08-31 19:08:36 +0300

    ~ try new settings

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -3,4 +3,4 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class LimitPageNumberPagination(PageNumberPagination):
-    page_size = settings.PAGE_SIZE
+    page_size = settings.REST_FRAMEWORK.PAGE_SIZE
