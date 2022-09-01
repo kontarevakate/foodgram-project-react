70f0df3fca95dc0d5e53a0a85da8657913b05d84
Екатерина Контарева <berbekaru.katerina@yandex.ru>
2022-09-01 11:05:06 +0300

    fix data

--- backend/recipes/management/commands/load_data.py
+++ backend/recipes/management/commands/load_data.py
@@ -1,4 +1,4 @@
-import csv
+import json
 import os
 
 from django.conf import settings
@@ -13,14 +13,14 @@ class Command(BaseCommand):
     help = 'loading ingredients from data in json or csv'
 
     def add_arguments(self, parser):
-        parser.add_argument('filename', default='ingredients.csv', nargs='?',
+        parser.add_argument('filename', default='ingredients.json', nargs='?',
                             type=str)
 
     def handle(self, *args, **options):
         try:
             with open(os.path.join(DATA_ROOT, options['filename']), 'r',
                       encoding='utf-8') as f:
-                data = csv.reader(f)
+                data = json.reader(f)
                 for row in data:
                     name, measurement_unit = row
                     Ingredient.objects.get_or_create(
