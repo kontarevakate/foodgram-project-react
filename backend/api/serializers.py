7ef9a550f7c5c74e9642cf5c8b67521ed8b33dfa
Екатерина Контарева <berbekaru.katerina@yandex.ru>
2022-09-01 14:52:56 +0300

    + get_is_in_shopping_cart

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -139,6 +139,12 @@ class RecipeReadSerializer(serializers.ModelSerializer):
             return False
         return Recipe.objects.filter(favorites__user=user, id=obj.id).exists()
 
+    def get_is_in_shopping_cart(self, obj):
+        user = self.context.get('request').user
+        if user.is_anonymous:
+            return False
+        return Recipe.objects.filter(cart__user=user, id=obj.id).exists()
+
 
 class CreateIngredientRecipeSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField()
