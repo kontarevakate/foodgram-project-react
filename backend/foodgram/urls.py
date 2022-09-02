f4fc0f046ac37ba5d982adb76505490b75e93a40
Екатерина Контарева <berbekaru.katerina@yandex.ru>
2022-09-02 16:48:26 +0300

    fix

--- backend/foodgram/urls.py
+++ backend/foodgram/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    path('api/auth/', include('djoser.urls.authtoken')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
