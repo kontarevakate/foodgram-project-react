f10cda227f8cb9ea0eeb069da05ff3ad0d624d1d
Екатерина Контарева <berbekaru.katerina@yandex.ru>
2022-09-02 12:31:55 +0300

    fix filters

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -12,12 +12,7 @@ class IngredientSearchFilter(SearchFilter):
 
 
 class RecipeFilter(FilterSet):
-    author = filters.ModelChoiceFilter(queryset=User.objects.all())
-    tags = filters.AllValuesMultipleFilter(
-        field_name='tags__slug',
-        to_field_name='slug',
-        queryset=Tag.objects.all()
-    )
+    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
     is_favorited = filters.NumberFilter(method='get_is_favorited')
     is_in_shopping_cart = filters.NumberFilter(
         method='get_is_in_shopping_cart'
