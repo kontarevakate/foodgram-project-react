92e0fe8af1164408521bd7e5344593488462a422
Екатерина Контарева <berbekaru.katerina@yandex.ru>
2022-09-02 13:03:58 +0300

    fix favorite

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,5 +1,5 @@
 from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Ingredient, Recipe, Tag, User
+from recipes.models import Ingredient, Recipe
 from rest_framework.filters import SearchFilter
 
 
